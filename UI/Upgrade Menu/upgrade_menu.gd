39e0b7d3acf2503db03035734c3a129ad5e470ba
james.sullivan <james.sullivan@axomic.com>
2024-04-13 14:32:27 -0400

    Upgrade menu gets the random upgrade from EventManager and sets icon accordingly. Also refactored to turn the icons into buttons.

--- UI/Upgrade Menu/upgrade_menu.gd
+++ UI/Upgrade Menu/upgrade_menu.gd
@@ -1,9 +1,21 @@
 extends Control
 @onready var confirmButton = $MarginContainer/VBoxContainer/PanelContainer/VBoxContainer/ConfirmButton
-# Called when the node enters the scene tree for the first time.
+@onready var player = $"../../Player"
+@onready var upgrade_slot_1 = $MarginContainer/VBoxContainer/PanelContainer/VBoxContainer/MarginContainer/HBoxContainer/MarginContainer/Upgrade1
+@onready var upgrade_slot_2 = $MarginContainer/VBoxContainer/PanelContainer/VBoxContainer/MarginContainer/HBoxContainer/MarginContainer2/Upgrade2
+@onready var upgrade_containers = [upgrade_slot_1,upgrade_slot_2]
+@onready var upgrades = [null,null]
+var damage_upgrade_icon = preload("res://art/icons/saw_blade.png")
+var fuel_upgrade_icon = preload("res://art/icons/fuel.webp")
+var upgrade_choice = null
+
+
+
 func _ready():
 	GameManager.connect("lawn_complete",on_lawn_complete)
-	print(EventManager.get_weighted_upgrade())
+	populate_upgrades()
+	#print(EventManager.get_weighted_upgrade())
+	#print(player.weapon.impact_damage)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,3 +32,23 @@ func _on_confirm_button_button_down():
 		visible = false
 	else: 
 		print('game not paused')
+		
+func populate_upgrades():
+	upgrades[0] = (EventManager.get_weighted_upgrade())
+	upgrades[1] = (EventManager.get_weighted_upgrade())
+	while upgrades[1] == upgrades[0]:
+		upgrades[1] = EventManager.get_weighted_upgrade()
+	for i in (upgrades.size()):
+		if upgrades[i] == 'fuel':
+			upgrade_containers[i].set_texture_normal(fuel_upgrade_icon)
+		elif upgrades[i] == 'damage':
+			upgrade_containers[i].set_texture_normal(damage_upgrade_icon)
+		elif upgrades[i] == 'health':
+			pass
+
+func _on_upgrade_1_button_down():
+	print(upgrades[0])
+
+
+func _on_upgrade_2_button_down():
+	print(upgrades[1])
